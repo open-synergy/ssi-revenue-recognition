a0496a0735de2159e30ca34620d3463ba3b26e78
Andhitia Rama <andhitia.r@gmail.com>
2023-01-20 19:28:42 +0700

    [ssi_revenue_recognition_full] 14.0.1.0.0

---
+++ ssi_revenue_recognition_full/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+
+from . import (
+    revenue_recognition_full_type,
+    revenue_recognition_full_type_account,
+    revenue_recognition_full,
+    revenue_recognition_full_account,
+)
