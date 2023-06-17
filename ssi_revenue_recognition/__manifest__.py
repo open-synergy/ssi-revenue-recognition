02b259cb9bee54b239d01f5f71ba64c99f6e37fc
Andhitia Rama <andhitia.r@gmail.com>
2023-06-17 23:07:22 +0700

    [ssi_revenue_recognition] 14.0.3.4.0

--- ssi_revenue_recognition/__manifest__.py
+++ ssi_revenue_recognition/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Revenue Recognition",
-    "version": "14.0.3.3.0",
+    "version": "14.0.3.4.0",
     "website": "https://simetri-sinergi.id",
     "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
     "license": "LGPL-3",
