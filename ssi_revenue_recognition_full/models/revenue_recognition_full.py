303586f2499e340546d4fc67b062f4dc32f83428
mikevhe18 <viriyananda.michael@gmail.com>
2024-02-07 19:01:47 +0700

    [ci] Change License To AGPL-3

--- ssi_revenue_recognition_full/models/revenue_recognition_full.py
+++ ssi_revenue_recognition_full/models/revenue_recognition_full.py
@@ -1,6 +1,6 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
 
 from odoo.addons.ssi_decorator import ssi_decorator
